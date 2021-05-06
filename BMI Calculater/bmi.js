012b70ebb7b95417bc604f800d391797dd9c103e
Veronika <nesteryuk15@gmail.com>
2021-05-06 18:43:02 +0300

    BMI Calculator Created

---
+++ BMI Calculater/bmi.js
@@ -0,0 +1,26 @@
+function calculate() {
+    let bmi;
+    let result = document.getElementById("result");
+
+    let weight = parseInt(document.getElementById("weight").value);
+    document.getElementById("weight-val").textContent = weight + " kg";
+
+    let height = parseInt(document.getElementById("height").value);
+    document.getElementById("height-val").textContent = height + " cm";
+
+    bmi = (weight / Math.pow( (height / 100), 2) ).toFixed(1);
+    result.textContent = bmi;
+
+    if( bmi < 18.5 ) {
+        category = "Underweight";
+        result.style.color = "0be881";
+    } else if( bmi >= 18.5 && bmi <= 24.9 ) {
+        category = "Normal weight";
+        result.style.color = "#ff884d";
+    } else if ( bmi >= 25 && bmi <= 29.9) {
+        category = "Obses";
+        result.style.color = "#ff5e57";
+    }
+
+    document.getElementById("category").textContent = category;
+}
